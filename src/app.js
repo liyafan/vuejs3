1538168b40628bb31aca44fa6dfda51ea2dba861
liyafan <liyafan0908@126.com>
2017-01-03 15:27:52 +0800

    project init

---
+++ src/app.js
@@ -0,0 +1,9 @@
+require('./styles/usage/app.scss');
+
+var common = require('./scripts/utils/util.common.js');
+//var html = require('./scripts/tpls/index.string');
+var html = require('./scripts/tpls/classify.string');
+
+common.render(html);
+
+//require('./scripts/views/index.js');
